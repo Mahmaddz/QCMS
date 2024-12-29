89d8274204b187170dc381dc876b9679b911158e
Mahmaddz <mahmadd.z1999@gmail.com>
2024-12-29 18:19:58 +0500

    small testing changes

--- backend/src/config/sphinxql.js
+++ backend/src/config/sphinxql.js
@@ -1,9 +1,13 @@
 const { Sphinxql } = require('sphinxql');
 const { sphinx } = require('./config');
 
-const sphql = Sphinxql.createConnection({
+const sphql = Sphinxql.createPoolConnection({
   host: sphinx.host,
   port: sphinx.port,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  multipleStatements: false,
 });
 
 module.exports = sphql;
